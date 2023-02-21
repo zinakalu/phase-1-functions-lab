d171f79d4ddcde9547a4e78c552a80c0a942c186
zinakalu <zina.kalu@gmail.com>
2023-02-21 16:28:52 -0500

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(){
+  return 1;
+}
+function distanceFromHqInBlocks(){
+  return 8;
+}
+const HQ_LOCATION = 42; 
+
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - HQ_LOCATION);
+}
+
+function distanceFromHqInFeet(location) {
+  const blocks = distanceFromHqInBlocks(location);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const feetPerBlock = 264;
+  return Math.abs(destination - start) * feetPerBlock;
+}
+
+
+
+function calculatesFarePrice (start, destination){
+let price = distanceTravelledInFeet(start, destination)
+if(price < 400){
+return 0;
+} else if(price > 400 && price < 2000){
+return ((price - 400) *2)/ 100;
+} else if (price > 2000 && price < 2500){
+  return 25
+} else {
+  return 'cannot travel that far';
+}
+}
